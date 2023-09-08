8bd461be907877324dbed08d4e36613472cd21b1
Antara Roy <116708061+AntaraRoy123456@users.noreply.github.com>
2023-09-08 23:03:08 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/software.jpg";
+import image from "../images/pics.jpg";
 
-const imageAltText = "Software related"
+const imageAltText = "Me on stage with prize."
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
